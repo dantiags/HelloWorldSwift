9f54fe35f06d18485d0253985f9d912c4b9fe69b
Carlos Dantiags <apple@macbook-de-carlos.local>
2016-08-09 12:35:45 -0300

    Initial Commit.....

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var displayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saySomethingTapped(sender: UIButton) {
+        displayLabel.text = "Hello World!"
     }
 
 
